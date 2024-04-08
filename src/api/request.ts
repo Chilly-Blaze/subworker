95156ccd8a3eee80251be1720061b749cea3ae5c
ChillyBlaze <l135163642@gmail.com>
2024-04-08 16:15:21 +0800

    fix scheduled

--- src/api/request.ts
+++ src/api/request.ts
@@ -7,7 +7,7 @@ const get = async (store: KVNamespace, name: string): Promise<string | null> =>
     await store.get(name)
 
 const assemblySubRequest = (env: Env, target: string, url: string) =>
-    `${env.BACKEND}${equals(last(env.BACKEND), '/') ? '' : '/'}sub?target=${target}&url=${url}&config=${env.CONFIG_ADDRESS}&emoji=false&add_emoji=false&list=false&xudp=false&udp=false&tfo=false&expand=true&scv=false&fdn=false&new_name=true`
+    `${env.BACKEND}${equals(last(env.BACKEND), '/') ? '' : '/'}sub?target=${target}&url=${url}&config=${env.CONFIG_ADDRESS}&emoji=false&add_emoji=false&list=false&xudp=false&udp=false&tfo=false&expand=true&sort=true&fdn=false&new_name=true`
 const assemblySubInfo = (subArr: number[]) =>
     `upload=${subArr[0]}; download=${subArr[1]}; total=${subArr[2]};` // expire=${subArr[3]}
 
--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import reset from './router/service/reset'
 export default {
     fetch: async (request: Request, env: Env): Promise<Response> =>
         router(request, env),
-    scheduler: async (_: Event, env: Env) => reset(env),
+    scheduled: async (_: Event, env: Env) => reset(env),
 }
